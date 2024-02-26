022f313f90333d467f1344b318db6b4d8ede7393
marlenetc <91817709+marlenetc@users.noreply.github.com>
2024-02-26 17:22:20 -0600

    Add files via upload

---
+++ src/main/java/com/example/p1holamundo/P1holamundoApplication.java
@@ -0,0 +1,24 @@
+package com.example.p1holamundo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class P1holamundoApplication implements CommandLineRunner {
+
+	private static Logger log = LoggerFactory.getLogger(P1holamundoApplication.class);
+	public static void main(String[] args) {
+
+		SpringApplication.run(P1holamundoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		/*System.out.println("Hola mundo!!! Soy Marlene TC en consola");*/
+		log.info("Hola mundo!!! Soy Marlene");
+	}
+}
